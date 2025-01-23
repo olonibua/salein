d000c4a9b27e107388d6bf2527eb1a88fb38babf
olonts <olonibua0001@gmail.com>
2025-01-23 14:40:29 +0000

    new changes

--- utils/appwrite.ts
+++ utils/appwrite.ts
@@ -1,7 +1,9 @@
 import { Client, Databases } from "appwrite";
 
 const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!);
+  .setEndpoint(
+    process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "http://your-default-endpoint"
+  )
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "");
 
 export const databases = new Databases(client);
