1b8df48d36bc82816c4f4734352c5e548e6b6aa0
Tolulope <113645248+olonibua@users.noreply.github.com>
2025-02-21 15:49:23 +0100

    Update route.ts

--- app/api/notifications/send/route.ts
+++ app/api/notifications/send/route.ts
@@ -56,7 +56,7 @@ export async function POST(req: Request) {
     console.log("Attachment size:", pdfBuffer.length);
 
     const emailData: EmailData = {
-      from: "Salein <onboarding@resend.dev>",
+      from: "Salein <Salein@olonts.site>",
       to: [to],
       cc: teamEmails,
       subject,
